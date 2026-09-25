5951a7471368675085a2bf59071b38636a08c78a
Robert Gabriel Mihai <roby07g@gmail.com>
2026-09-25 16:39:07 +0200

    Implement data cleaning in the Silver layer and optimize ingestion frequency.

---
+++ silver.py
@@ -0,0 +1,39 @@
+from pyspark.sql import SparkSession 
+
+spark = SparkSession \
+    .builder \
+    .appName("Silver Layer Processor") \
+    .getOrCreate()
+
+df_bronze = spark.read \
+    .format("parquet") \
+    .load("./datalake/bronze/flights")
+
+df_bronze.createOrReplaceTempView("bronze_flight_telemetry")
+
+clean_query = """
+    SELECT
+        icao24, 
+        callsign,
+        date, 
+        origin_country, 
+        longitude, 
+        latitude, 
+        velocity, 
+        true_track, 
+        baro_altitude,
+        on_ground,
+        vertical_rate
+    FROM bronze_flight_telemetry
+    WHERE longitude IS NOT NULL AND latitude IS NOT NULL;
+"""
+
+df_silver = spark.sql(clean_query)
+
+df_silver.write \
+    .format("parquet") \
+    .mode("overwrite") \
+    .partitionBy("date") \
+    .save("./datalake/silver/flights")
+
+print("Datos procesados y guardados en la capa Silver con exito.")
